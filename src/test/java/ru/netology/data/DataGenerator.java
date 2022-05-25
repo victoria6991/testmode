f4903235f199513b6fae64b44527d39d59b77e59
victoria6991 <miss.victoria-liar@yandex.ru>
2022-05-25 20:23:02 +0300

    lombok fixed

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -66,24 +66,6 @@ public class DataGenerator {
 
     @Value
     public static class RegistrationDto {
-        public RegistrationDto(String login, String password, String status) {
-            this.login = login;
-            this.password = password;
-            this.status = status;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
         String login;
         String password;
         String status;
